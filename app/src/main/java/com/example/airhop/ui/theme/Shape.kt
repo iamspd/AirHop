10d408e995c78ec7732644efd72d7323ed876506
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-03-25 17:44:11 -0400

    Add dimens.xml, strings.xml and Shapes resource file

---
+++ app/src/main/java/com/example/airhop/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package com.example.airhop.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(topEnd = 16.dp),
+    medium = RoundedCornerShape(24.dp)
+)
