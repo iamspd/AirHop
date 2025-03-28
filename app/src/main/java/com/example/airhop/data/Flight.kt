40336afcd68b701c37cfdee96ed5c6d2f6d69026
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-03-28 16:00:50 -0400

    Add Flight data class

--- app/src/main/java/com/example/airhop/data/Flight.kt
+++ app/src/main/java/com/example/airhop/data/Flight.kt
@@ -1,8 +1,10 @@
 package com.example.airhop.data
 
+/**
+ * Data class that represents flight information based on
+ * [departureAirport] and [destinationAirport].
+ */
 data class Flight(
-    val departureCode: String,
-    val departureName: String,
-    val destinationCode: String,
-    val destinationName: String
+    val departureAirport: Airport,
+    val destinationAirport: Airport
 )
