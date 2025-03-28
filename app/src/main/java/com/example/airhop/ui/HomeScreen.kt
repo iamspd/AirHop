d850946697e44046aad855eaaf74d9db0e97250e
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-03-28 17:20:12 -0400

    Remove unused imports

--- app/src/main/java/com/example/airhop/ui/HomeScreen.kt
+++ app/src/main/java/com/example/airhop/ui/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.airhop.ui
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.airhop.AppViewModelProvider
